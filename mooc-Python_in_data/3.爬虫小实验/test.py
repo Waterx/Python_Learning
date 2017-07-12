9945a6eb94ff303d1b509cfacdf262a0e4057b7b
waterx <hzx915184874@gmail.com>
2017-07-12 22:00:16 +0800

    modify star

--- mooc-Python_in_data/3.爬虫小实验/test.py
+++ mooc-Python_in_data/3.爬虫小实验/test.py
@@ -8,9 +8,10 @@ r = requests.get('https://book.douban.com/subject/1082154/comments/')
 soup = BeautifulSoup(r.text, 'lxml')
 pattern = soup.find_all('p', 'comment-content')
 for item in pattern:
-    print(item.string,'\n')
-pattern_s = re.compile('<span class="user-stars allstar(.*)rating" title="力荐"></span>')
+    print(item.string, '\n')
+pattern_s = re.compile('<span class="user-stars allstar(.*)rating"')
 p = re.findall(pattern_s, r.text)
 for star in p:
     sum += int(star)
 print(sum)
+
