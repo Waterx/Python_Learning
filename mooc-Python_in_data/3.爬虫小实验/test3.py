2a3928f2f82660db78576d6b4453f38566776c12
waterx <hzx915184874@gmail.com>
2017-07-17 14:41:27 +0800

    go on

--- mooc-Python_in_data/3.爬虫小实验/test3.py
+++ mooc-Python_in_data/3.爬虫小实验/test3.py
@@ -7,14 +7,14 @@ sum = 0
 r = requests.get('http://money.cnn.com/data/dow30/')
 soup = BeautifulSoup(r.text, 'lxml')
 pattern = soup.find_all('a', 'wsod_symbol')
-pattern2 = soup.find_all('span')
+pattern2 = soup.find_all('tr')
 i = 0
 for item in pattern:
     print(i+1)
     print(item.string, '\n')
     i = i + 1
 for item in pattern2:
-    print(item.string, '\n')
+    print(item, '\n')
 # pattern_s = re.compile('<span class="user-stars allstar(.*)rating"')
 # p = re.findall(pattern_s, r.text)
 # for star in p:
