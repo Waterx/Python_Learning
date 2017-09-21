a75c8538e5fbaece5380642969a77b5986f6efa2
waterx <hzx915184874@gmail.com>
2017-09-21 20:13:47 +0800

    initial commit

---
+++ book_THW_Python/ex10.py
@@ -0,0 +1,17 @@
+# 制表符的使用对于格式化输出字符非常有用
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm spilt\non a line"
+backslash_cat = "I'm \\ a \\ cat"
+
+fat_cat = '''
+I'll do a list:
+\t* Cat food \t* Ball
+\t* Fishies \t* Mice
+\t* Catnip\n\t* Grass
+'''
+
+print tabby_cat
+print persian_cat
+print backslash_cat
+print fat_cat
---
+++ book_THW_Python/ex9.py
@@ -0,0 +1,9 @@
+#'''包围起来的字符串里面换行的时候其实里面包含一个\n
+
+x = '''
+There's sth going on here
+With the three double-quotes
+We'll be able to type as much as we like
+'''
+
+print '%r' % x
