7d8b1e6e868dd0ea562b39823404367b40144ba3
waterx <hzx915184874@gmail.com>
2017-09-26 10:47:21 +0800

    initial commit, but not done yet

---
+++ book_THW_Python/ex49/skeleton/setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+config = {
+    'description': 'My Project',
+    'author': 'Don',
+    'url': 'URL to get it at.',
+    'download_url': 'Where to download it.',
+    'author_email': 'don@gmail.com',
+    'version': '0.1',
+    'install_requires': ['nose'],
+    'packages': ['NAME'],
+    'scripts': [],
+    'name': 'projectname'
+}
+
+setup(**config)
+
